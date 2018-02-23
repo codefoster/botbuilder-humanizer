71fee3c62dee2170a5269bde4025e211e831a418
Brandon Hurlburt <hurlburb@microsoft.com>
2018-02-23 11:01:25 -0800

    middleware port complete

---
+++ csharp/BotBuilder.Humanizer/BotBuilder.Humanizer.Middleware/Humanizer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Middleware;
+using Microsoft.Bot.Schema;
+
+namespace BotBuilder.Humanizer.Middleware
+{
+    public partial class Humanizer : IMiddleware, IReceiveActivity
+    {
+        private static IList<string> _wrongPersonStatements = new[]
+        {
+            "not a chance",
+            "thursday",
+            "I said 7!!",
+            "thbbbbpt!"
+        };
+
+        private readonly double _misspelling, _wrongPerson, _transpositions, _typingSpeed;
+        public Humanizer(HumanizerOptions options)
+        {
+            _misspelling = options.Misspelling;
+            _wrongPerson = options.WrongPerson;
+            _transpositions = options.Transpositions;
+            _typingSpeed = options.TypingSpeed;
+        }
+
+        public async Task ReceiveActivity(IBotContext context, MiddlewareSet.NextDelegate next)
+        {
+            await next();
+
+            foreach (var response in context.Responses.OfType<IMessageActivity>())
+            {
+                var delay = response.Text.Split(' ').Length / (_typingSpeed / 60 / 1000);
+
+                // transpositions
+                if (new Random().NextDouble() < _transpositions)
+                {
+                    var text = response.Text.ToCharArray();
+                    var position = this.Random(0, text.Length - 2);
+                    var aux = text[position + 1];
+                    text[position + 1] = text[position];
+
+                    response.Text = new string(text);
+                }
+
+                // wrongPerson
+                if (new Random().NextDouble() < _wrongPerson)
+                {
+                    TypingDelay(context, delay)
+                        .Reply(_wrongPersonStatements[Random(0, _wrongPersonStatements.Count - 1)]);
+                    TypingDelay(context, delay)
+                        .Reply("sorry... that last message was meant for someone else");
+                }
+
+                // misspelling
+                foreach (var word in response.Text.Split(' '))
+                {
+                    if (_misspellings.TryGetValue(word, out var replacement) && new Random().NextDouble() < _misspelling)
+                    {
+                        response.Text = response.Text.Replace(word, replacement);
+                        TypingDelay(context, delay)
+                            .Reply($@"{word}*");
+                    }
+                }
+            }
+        }
+
+        private IBotContext TypingDelay(IBotContext context, double msDelay) => context.ShowTyping().Delay((int)msDelay);
+
+        private int Random(double a, double b) => (int)Math.Round(b + (new Random().Next() * (a - b)));
+    }
+}
